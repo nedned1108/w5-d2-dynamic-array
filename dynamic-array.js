a757d9022e8f4572482086d261d27ab2309a0113
nednguyen <rpmshockwave@gmail.com>
2022-10-25 23:53:59 -0400

    finished

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,46 +1,69 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(this.capacity);
   }
 
   read(index) {
 
-    // Your code here
+    return this.data[index]
   }
 
   push(val) {
-
-    // Your code here
+    if (this.length === this.capacity) {
+      this.resize()
+    }
+    this.data[this.length] = val;
+    return this.length++; 
   }
 
 
   pop() {
-
-    // Your code here
-  }
-
-  shift() {
-
-    // Your code here
+    this.data[this.length] = null;
+    return this.length--
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length === this.capacity) {
+      this.resize()
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++
+    return this.length;
+  }
+  
+  shift() {
+    let remove = this.data[0];
+    for (let i = 0; i < this.capacity - 1; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+    this.length--;
+    return remove;
   }
 
   indexOf (val) {
-
-    // Your code here
+    for (let i = 0; i < this.capacity; i++) {
+      if (val === this.data[i]) {
+        return i
+      }
+    }
+    return -1
   }
 
   resize () {
-
-    // Your code here
+    this.capacity *= 2;
+    let newArr = new Array(this.capacity)
+    for (let i = 0; i < this.capacity; i++) {
+      newArr[i] = this.data[i]
+    }
+    return this.data = newArr;
+    
   }
-
 }
 
 
